953269be649a4df52e4e5a6c180cc85ad9a64782
user012100 <asalifov@gmail.com>
2025-11-05 20:01:39 -0500

    fixed hopefully

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Flat setup, no folders
 import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
 import { OrbitControls } from 'https://unpkg.com/three@0.160.0/examples/jsm/controls/OrbitControls.js';
 
